4d7b63e8921167252b7260ccf95a3f302e81fae6
Mohamed Osama <mohamedossama513@gmail.com>
2026-08-06 11:40:08 +0300

    feat: update phone number validation to support local formats

--- svc-transactions/util/common/helpers.go
+++ svc-transactions/util/common/helpers.go
@@ -7,12 +7,12 @@ import (
 
 func ValidatePhoneNumber(phoneNumber *string) error {
 	*phoneNumber = strings.TrimSpace(*phoneNumber)
-	if !strings.HasPrefix(*phoneNumber, "+20") || len(*phoneNumber) != 13 {
+	if !(strings.HasPrefix(*phoneNumber, "010") || strings.HasPrefix(*phoneNumber, "011") || strings.HasPrefix(*phoneNumber, "012") || strings.HasPrefix(*phoneNumber, "015")) || len(*phoneNumber) != 11 {
 		return transactions.ErrInvalidPhoneNumber // return the domain error for invalid phone number format
 	}
 
 	// 2. Verify the payload is numeric (prevents +20ABCDEFGHIJ)
-	for _, ch := range (*phoneNumber)[3:] {
+	for _, ch := range (*phoneNumber) {
 		if ch < '0' || ch > '9' {
 			return transactions.ErrInvalidPhoneNumber // return the domain error for invalid phone number format
 		}
